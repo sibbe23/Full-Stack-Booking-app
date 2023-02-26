8605e4b570be6747a9dfba87734bbb0205ff8f5b
sibbe23 <sibbe23@gmail.com>
2023-02-26 11:37:16 +0530

    App Complete-ready for Demo

--- public/js/booking.js
+++ public/js/booking.js
@@ -3,30 +3,19 @@ const local = document.querySelector(".local");
 const names = document.getElementById("name");
 const emails = document.getElementById("email");
 const phones = document.getElementById("phone");
-document
-  .querySelector("form.formdata")
-  .addEventListener("submit", function (e) {
+document.querySelector("form.formdata").addEventListener("submit", function (e) {
     e.preventDefault();
-    const x = document.querySelector("form.formdata").elements;
-    const user_obj = {
-      name: x["name"].value,
-      email: x["email"].value,
-      phone: x["phone"].value,
-    };
+    const element = document.querySelector("form.formdata").elements;
+    const user_obj = {name: element["name"].value,email: element["email"].value,phone: element["phone"].value};
     window.location.reload();
     if (!names.title) {
-      axios
-        .post("http://localhost:3000/booking", user_obj)
+        axios.post("http://localhost:3000/booking", user_obj) // sending the data
         .then((data) => {
           const div = document.createElement("div");
           div.className = "d-inline-flex p-2";
           const p = document.createElement("p");
-          div.setAttribute("id", data.data._id);
-          p.appendChild(
-            document.createTextNode(
-              data.data.name + " " + data.data.email + " " + data.data.phone
-            )
-          );
+          div.setAttribute("id", data.user_obj._id);
+          p.appendChild(document.createTextNode(data.name + " " + data.email + " " + data.phone));
           const btn = document.createElement("button");
           btn.className = "del_button";
           btn.appendChild(document.createTextNode("Delete"));
@@ -38,39 +27,30 @@ document
           div.appendChild(btn1);
           di.appendChild(div);
         })
-        .catch((err) => console.log(err));
-    } else {
-      axios
-        .put("http://localhost:3000/" + names.title, user_obj)
+        .catch((err) => console.log(err));}
+        else {
+         axios.put("http://localhost:3000/" + names.title, user_obj) //incase we need to update the data we ue put
         .then((data) => {
           const div = document.createElement("div");
           div.className = "d-inline-flex p-2";
           const p = document.createElement("p");
           div.setAttribute("id", data.data._id);
-          p.appendChild(
-            document.createTextNode(
-              data.data.name + " " + data.data.email + " " + data.data.phone
-            )
-          );
+          p.appendChild(document.createTextNode(data.name + " " + data.email + " " + data.phone));
           const btn = document.createElement("button");
           btn.className = "del_button";
           btn.appendChild(document.createTextNode("Delete"));
           const btn1 = document.createElement("button");
           btn1.className = "edit_btn";
           btn1.appendChild(document.createTextNode("Edit"));
-          div.appendChild(p);
-          div.appendChild(btn);
-          div.appendChild(btn1);
-          di.appendChild(div);
+          div.appendChild(p);div.appendChild(btn);div.appendChild(btn1);di.appendChild(div);
           window.location.reload();
         })
         .catch((err) => console.log(err));
     }
   });
-//Fetch all data on window loaded
-window.addEventListener("DOMContentLoaded", () => {
-  axios
-    .get("http://localhost:3000")
+//Fetch all data
+     window.addEventListener("DOMContentLoaded", () => {
+     axios.get("http://localhost:3000") //using axios to get the data 
     .then((res) => {
       const data = res.data.appointments;
       for (let i = 0; i < data.length; i++) {
@@ -78,11 +58,7 @@ window.addEventListener("DOMContentLoaded", () => {
         div.className = "d-inline-flex p-2";
         const p = document.createElement("p");
         div.setAttribute("id", data[i].id);
-        p.appendChild(
-          document.createTextNode(
-            data[i].name + " " + data[i].email + " " + data[i].phone
-          )
-        );
+        p.appendChild(document.createTextNode(data[i].name + " " + data[i].email + " " + data[i].phone));
         const btn = document.createElement("button");
         btn.className = "del_button";
         btn.style="width: 25%; border-width: thin; border-radius: 5px;border-color: lightgray; border-style:solid; color: red;";
@@ -91,35 +67,26 @@ window.addEventListener("DOMContentLoaded", () => {
         btn1.className = "edit_btn";
         btn1.style="width: 25%; border-width: thin; border-radius: 5px;border-color: lightgray; border-style:solid; color: grey;";
         btn1.appendChild(document.createTextNode("Edit"));
-        div.appendChild(p);
-        div.appendChild(btn);
-        div.appendChild(btn1);
-        di.appendChild(div);
-        
-      }
-      
+        div.appendChild(p);div.appendChild(btn); div.appendChild(btn1); di.appendChild(div);       
+      }  
     })
     .catch((err) => console.log(err));
 });
-
 //delete data server
 local.addEventListener("click", delete_data);
 function delete_data(e) {
   if (e.target.classList.contains("del_button")) {
     window.location.reload();
-    axios
-      .delete("http://localhost:3000/" + e.target.parentElement.id)
+    axios.delete("http://localhost:3000/" + e.target.parentElement.id)
       .then((res) => {
         console.log("res", res);
-        
         console.log(e.target.parentElement);
         di.removeChild(e.target.parentElement);
       })
       .catch((err) => console.log(err));
   }
 }
-//edit local storage data
-
+//edit local storage 
 local.addEventListener("click", edit_data);
 function edit_data(e) {
   if (e.target.classList.contains("edit_btn")) {
